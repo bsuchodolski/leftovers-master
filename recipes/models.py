92ab01e0cc719df20bbb34ca859c9623654711ac
Bartek S <suchodolski.bartosz@gmail.com>
2017-02-18 20:22:45 +0100

    Add home view and models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey('Ingredient')
+    recipe = models.ForeignKey('Recipe')
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return '{0} {1}{2}'.format(self.ingredient.name, self.quantity, self.ingredient.unit)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=20)
+    unit = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
